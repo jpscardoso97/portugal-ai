5051d0b8d039d855ed07cc4b5302bb9d72af5e75
jpscardoso97 <j040_k4rd050@hotmail.com>
2024-07-08 17:04:08 -0400

    Add dockerfiles and new workflow

--- PortugalAi/App/HttpClients/LLMHttpRequestHandler.cs
+++ PortugalAi/App/HttpClients/LLMHttpRequestHandler.cs
@@ -2,13 +2,16 @@ namespace App.HttpClients;
 
 public class LlmHttpRequestHandler: HttpClientHandler
 {
+    private const string host = "http://host.docker.internal:8080";
+    //private const string host = "http://localhost:8080";
+
     protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
         CancellationToken cancellationToken)
     {
         if (request.RequestUri != null &&
             request.RequestUri.Host.Equals("api.openai.com", StringComparison.OrdinalIgnoreCase))
         {
-            request.RequestUri = new Uri($"http://localhost:8080{request.RequestUri.PathAndQuery}");
+            request.RequestUri = new Uri($"{host}{request.RequestUri.PathAndQuery}");
         }
         
         return base.SendAsync(request, cancellationToken);
